fbc3d4c348939e391cd148223416bf88130a0540
Rokossovsky <123229180+TRokossovsky@users.noreply.github.com>
2023-06-06 04:58:49 +0300

    Improved and commented

--- gelbooru_scraper.py
+++ gelbooru_scraper.py
@@ -2,7 +2,7 @@ import json
 import asyncio
 from python_gelbooru import AsyncGelbooru
 
-with open('./Testing/APIs.txt', 'r') as f:
+with open('./Testing/APIs.txt', 'r') as f: # put your gelbooru API in similar file
     data = f.read()
     js = json.loads(data)
 api_key, user_id = (js['api_key'], js['user_id'])
@@ -11,8 +11,7 @@ api_key, user_id = (js['api_key'], js['user_id'])
 async def main():
     async with AsyncGelbooru(api_key=api_key,
                              user_id=user_id) as gel:
-        yuyu = await gel.search_posts(['as109'], limit=999, random=True)
-
+        yuyu = await gel.search_posts([''], limit=999, random=True) # put needed tag into braces
         tasks = [i.async_download(f"./arts/{i.id}") for i in yuyu]
         await asyncio.gather(*tasks)
 
@@ -21,4 +20,4 @@ while True:
         loop = asyncio.get_event_loop()
         loop.run_until_complete(main())
 
-# scraps up to 100 elements in a row. launch with random=True until it collects all.
+# scraps up to 100 elements in a row. launch with random=True over and over again until it collects all.
